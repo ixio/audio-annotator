decb0a4f57eb5c14ed927fb04b72a38fb17ddb16
Erwan Keribin <erwan.keribin@gmail.com>
2019-01-16 19:15:55 +0100

    Disable annotation and changes legend

--- static/js/src/main.js
+++ static/js/src/main.js
@@ -66,7 +66,7 @@ function Annotator() {
     // Create the annotation stages that appear below the wavesurfer. The stages contain tags 
     // the users use to label a region in the audio clip
     this.stages = new AnnotationStages(this.wavesurfer, this.hiddenImage);
-    this.stages.create();
+    //this.stages.create();
 
     // Create Workflow btns (submit and exit)
     this.workflowBtns = new WorkflowBtns();
